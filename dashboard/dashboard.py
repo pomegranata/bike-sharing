016a69504541b3776c29e7c408afd81eb8b5fac7
pomegranata <imeldamaretta.putri346@gmail.com>
2024-09-30 22:29:20 +0700

    plot fix

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -214,7 +214,7 @@ tab1, tab2 = lit.tabs(["Bulanan", 'Tahunan'])
 
 # Bulanan
 with tab1:
-    fig1, ax1 = plot.subplots(figsize=(12, 6))
+    fig1, ax1 = plot.subplots(figsize=(10, 5))
     ulpipi.lineplot(
         x='mnth',
         y='cnt',
